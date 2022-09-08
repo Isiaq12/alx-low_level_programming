73098ad06676e635b3fc8b3f5ed7c71feaa94ae3
Isiaq12 <salausiaka1998@gmail.com>
2022-09-08 14:09:30 -0700

    sweat

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-	int 4;
-	double 8;
-	char 1;
-
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(4));
-	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(8));
-	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(1));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

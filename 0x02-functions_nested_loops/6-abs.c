cfbe2479d9c227c82d5ceb913ac951a0bc5d7fa6
Isiaq12 <salausiaka1998@gmail.com>
2022-09-13 19:13:51 -0700

    g

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _abs - compute absolute value of an integer
+ *
+ * @int: any value
+ *
+ * Return: absolutely
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}

302f065b61697b2177d197e1c14dabe67a58c1b2
Isiaq12 <salausiaka1998@gmail.com>
2022-09-14 07:48:01 -0700

    j

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,13 @@
 /**
  * add - to sum two integer together
  *
+ * @i: first number
+ *
+ * @j: second digit
+ *
  * Return: Always 0.
  */
 int add(int i, int j)
 {
-	return(i + j);
+	return (i + j);
 }

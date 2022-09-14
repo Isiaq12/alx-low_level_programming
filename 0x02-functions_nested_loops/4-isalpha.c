09407df2a968c49008a734693b320b794fb4e881
Isiaq12 <salausiaka1998@gmail.com>
2022-09-13 17:50:10 -0700

    alphanumeric

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _isalpha - check of alphabetic character
+ *
+ * @c: name of the integer
+ *
+ * Return: Success
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

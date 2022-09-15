8aa22e0993ce28b12cb0becad88ca39fafa5a8e1
Isiaq12 <salausiaka1998@gmail.com>
2022-09-15 16:32:04 -0700

    thanks

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: measurement
+ *
+ * Return: Success
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 1 ; i <= size ; i++)
+	{
+	for (j = 1 ; j >= i ; j++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz ");
+		continue;
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+		continue;
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+		continue;
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+	}
+	printf("\n");
+}

9dbd4138a33ea928ec51b5ae1adda02f08698060
Isiaq12 <salausiaka1998@gmail.com>
2022-09-15 05:45:23 -0700

    no

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_numbers - to display digit
+ *
+ * Return: Success
+ */
+void print_numbers(void)
+{
+	int k = 0;
+
+	while (k <= 9)
+	{
+		_putchar(k);
+		k++;
+	}
+	_putchar('\n');
+}

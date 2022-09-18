49d0dcf5960e731e1b96c6879400057648f8217c
Isiaq12 <salausiaka1998@gmail.com>
2022-09-18 15:55:14 -0700

    go

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_number - to display any digit
+ *
+ * @n: name of the variable
+ *
+ * Return: Success
+ */
+void print_number(int n)
+{
+	if (n < 0 || n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return(0);
+	}
+}

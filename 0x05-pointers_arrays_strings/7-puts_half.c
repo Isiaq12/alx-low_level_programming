da5e8ba743303c6f82310ec5dc353efce041b48d
Isiaq12 <salausiaka1998@gmail.com>
2022-09-21 16:57:34 -0700

    correct

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,22 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int i, j;
 	int n = (_strlen(str)) - 1 / 2;
 
 	if ((_strlen(str)) % 2 != 0)
 	{
-		_putchar('0' + n);
+	for (j = n ; j < (_strlen(str)) ; j++)
+	{
+		_putchar(str[j]);
 	}
+	}
+	else
+	{
 	for (i = (_strlen(str)) / 2 ; i < (_strlen(str)) ; i++)
 	{
 		_putchar(str[i]);
+	}
 	}
 	_putchar('\n');
 }

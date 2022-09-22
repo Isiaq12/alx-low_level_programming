d7098b2e65b1f4de35030b0bfeef16cde257026a
Isiaq12 <salausiaka1998@gmail.com>
2022-09-22 10:47:10 -0700

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, value;
+	int i, value = 0;
 
 	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			value = s1[i] - s2[i];
-
 			break;
 		}
 	}

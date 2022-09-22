9dcdac891b513bfa19a82ea18d3d14324b87d07f
Isiaq12 <salausiaka1998@gmail.com>
2022-09-22 07:09:07 -0700

    no erro

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _strncpy - to copy a string to another string
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: n (bytes) of character
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] = '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}

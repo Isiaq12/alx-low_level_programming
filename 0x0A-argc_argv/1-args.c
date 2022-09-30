f14c58645fb605bcfaee01199b7f1ea537a9abf8
Isiaq12 <salausiaka1998@gmail.com>
2022-09-30 10:32:58 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc - argument count
+ *
+ * @argv - argument string passed
+ *
+ * Return: Always (0) success
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

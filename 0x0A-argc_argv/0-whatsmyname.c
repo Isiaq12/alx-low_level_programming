8551dd89a5996d8b9b562670868f9be01087e97b
Isiaq12 <salausiaka1998@gmail.com>
2022-09-30 10:08:21 -0700

    my first argument to main project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: argument count
+ *
+ * @argv: arrays of string of argument
+ *
+ * Return: always (0) success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

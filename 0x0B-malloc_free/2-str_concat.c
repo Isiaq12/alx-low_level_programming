28344073eed4c5bc972be70e65b1c70fce322d72
Isiaq12 <salausiaka1998@gmail.com>
2022-10-04 09:20:30 -0700

    kunle

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,26 +14,43 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	unsigned int i;
-	unsigned int len = 0;
+	unsigned int i, k;
+	unsigned int len1, len2;
+	int total;
 
 	if (s1 == NULL)
 	{
-		return ('\0');
+		s1 = "";
 	}
-	while (*(s1 + len) != '\0')
+	if (s2 == NULL)
 	{
-		size += len++;
+		s2 = "";
 	}
-	concat = malloc(size * sizeof(*s1));
+	while (s1[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	while (s2[k] != '\0')
+	{
+		len2++;
+		k++;
+	}
+	total = (len2 + len1);
+	concat = malloc(sizeof(*s1) * total);
 	if (concat == NULL)
 	{
 		return ('\0');
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < len1; i++)
 	{
-		concat[size + i] = s2[i];
+		concat[i] = s1[i];
+	}
+	for (k = 0; k < len2; k++)
+	{
+		concat[len1 + k] = s2[k];
 	}
 	return (concat);
 }
+
 

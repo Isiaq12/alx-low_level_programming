49276b720ba95f8ada0bf35ef573ee782c9bf0a3
Isiaq12 <salausiaka1998@gmail.com>
2022-10-10 13:26:37 -0700

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - data type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: This structure contains three members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

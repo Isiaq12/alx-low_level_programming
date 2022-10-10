8f4dda3174b924ca29f0cd18a9f08a16fd61269d
Isiaq12 <salausiaka1998@gmail.com>
2022-10-10 15:46:42 -0700

    renew

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,33 +3,20 @@
 
 /**
  * struct dog - data type
+ * dog_t - alias for data type
  * @name: first member
  * @age: second member
  * @owner: third member
  *
  * Description: This structure contains three members
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-/**
- * dog_t - new name
- * struct dog - old name for data type
- * @name: first member
- * @age: second member
- * @owner: third member
- *
- * Description: this structure contains three members and new name for data type*/
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif

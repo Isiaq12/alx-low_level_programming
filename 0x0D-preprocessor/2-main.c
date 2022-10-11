87ec522274bd3981799d6a66e7d621771271c9b9
Isiaq12 <salausiaka1998@gmail.com>
2022-10-11 10:35:51 -0700

    everything at once

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - check the cocde.
+ *
+ * To print out the name of the file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

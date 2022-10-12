ed93350503c22f075385102c54d81be8c9ea65e2
Isiaq12 <salausiaka1998@gmail.com>
2022-10-12 16:09:49 -0700

    go

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-#endif /*FUNCTION_POINTERS_H*/
+#endif /*FUNCTION_POINTERS_*/

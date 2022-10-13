2329eddcdead9cccb2b9722d36528f6d26861c84
Isiaq12 <salausiaka1998@gmail.com>
2022-10-12 17:15:19 -0700

    kunle

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,23 +2,22 @@
 #include"function_pointers.h"
 
 /**
- * array_iterator - to passed each element of the array to a given function
+ * array_iterator - to passed each element of the array to a given parameter
+ *
  * @array: the array
  * @size: the size of the array
  * @action: pointer to a function
  *
- * Return: Always Success.
+ * Return: void
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int));
 {
 	int i;
 
-	if (!action || !array)
+	if (action != NULL || array != NULL)
 		return;
-
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
 	}
 }

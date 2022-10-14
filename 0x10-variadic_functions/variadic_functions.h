1c54c26752094d5978e48f969ca1316fb858d04d
Isiaq12 <salausiaka1998@gmail.com>
2022-10-13 19:06:03 -0700

    biggest function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,18 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct format_struct - format_type class
+ * @format: format identifier
+ * @fmt_print_func: pointer to function
+ */
+
+typedef struct format_struct
+{
+	char format;
+	void (*fmt_print_func)(va_list *arg_list);
+} format_struct_ptr;
 
 #endif /*VARIADIC_FUNCTION_H*/

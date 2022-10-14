f0edbfa52c55dc073f5335638612be39117faae6
Isiaq12 <salausiaka1998@gmail.com>
2022-10-13 17:24:46 -0700

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include"variadic_functions.h"
+
+/**
+ * print_numbers - to print out numbers
+ *
+ * @separator: puntuation or anything, string to be precise
+ * @n: number of string
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list kunle;
+	int x;
+
+	if (separator == NULL)
+	{
+		printf(" ");
+	}
+	va_start(kunle, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(kunle, int);
+		printf("%d", x);
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(kunle);
+}
